381e3043bcbf627dc32feb0ac0e966317fdd1289
Andrew <matruneckyi@gmail.com>
2025-08-12 00:23:05 +0300

    С-003 Исправление типа эррей для корректной работы openApi, небольшие правки зависимостей и сахар

--- src/main/java/com/example/crud/domain/Employee.java
+++ src/main/java/com/example/crud/domain/Employee.java
@@ -3,11 +3,13 @@ package com.example.crud.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "employees")
 @Data
 @NoArgsConstructor
+@Schema(description = "Сущность сотрудника")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
